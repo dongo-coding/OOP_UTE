ce55411e43708a221ed167ac1f72552759501896
Do Ngo <bahuynh619@gmail.com>
2025-08-24 08:07:17 +0700

    Update Circle.cs

--- Circle.cs
+++ Circle.cs
@@ -1,11 +1,13 @@
 using System;
 class Circle {
   static void Main() {
-    Console.Write("Nhap ban kinh cua duong tron : ")
+    Console.Write("Nhap ban kinh cua duong tron : ");
     double r = Convert.ToDouble(Console.ReadLine());
     double pi = 3.14;
     double chuvi = 2 * pi * r;
-    Console.Write("Chu vi cua duong tron la : " + chuvi);
+    double dientich = pi * Math.Pow(r,2);
+    Console.WriteLine("Chu vi cua duong tron la : " + chuvi);
+    Console.Write("Dien tich cua duong tron la : " + dientich);
     Console.ReadLine();
   }
 }
